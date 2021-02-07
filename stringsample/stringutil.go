dab5458583af85d1001c8b02cc9bdf7ef209b265
Kimi-Qu <eyres.jones@gmail.com>
2021-02-07 13:28:08 +0800

    code changed

---
+++ stringsample/stringutil.go
@@ -0,0 +1,16 @@
+package stringsample
+
+import "fmt"
+
+func init() {
+	fmt.Println("init of stringsample package")
+}
+
+// Reverse returns its argument string reversed rune-wise left to right.
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
