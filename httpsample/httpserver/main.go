edbb9ad99555e45cd648a54c18a1c0e3ea2b100f
Kimi-Qu <eyres.jones@gmail.com>
2021-02-20 17:10:50 +0800

    code changed

--- httpsample/httpserver/main.go
+++ httpsample/httpserver/main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"net/http/httputil"
 )
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
 	message := "hello world"
 	if _, err := w.Write([]byte(message)); err != nil {
-		fmt.Println(err)
+		log.Println(err)
+	}
+}
+
+func traceLog(w http.ResponseWriter, r *http.Request) {
+	if body, err := ioutil.ReadAll(r.Body); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	} else {
+		log.Println(string(body))
+	}
+}
+
+func trace(w http.ResponseWriter, r *http.Request) {
+	if payload, err := httputil.DumpRequest(r, true); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	} else {
+		log.Println(string(payload))
 	}
 }
 
 func main() {
 	http.HandleFunc("/sayhello", sayHello)
-	if err := http.ListenAndServe("127.0.0.1:3000", nil); err != nil {
-		fmt.Println(err)
+	http.HandleFunc("/log", traceLog)
+	http.HandleFunc("/log1", trace)
+	if err := http.ListenAndServe("0.0.0.0:3000", nil); err != nil {
+		log.Fatalln(err)
 	}
 }
